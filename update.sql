24557ea1ea7cd3b7d2f3894f419d0ab6c2d926e5
JavierRodriguezdeCea <javier.rodriguez.decea@outlook.com>
2024-11-09 13:49:04 +0100

    correccion de errores3

--- update.sql
+++ update.sql
@@ -1,13 +1,5 @@
 USE lab_mysql;
 
-UPDATE customers
-SET cust_email = 'ppicasso@gmail.com'
-WHERE cust_name  = 'Pablo Picasso';
-
-UPDATE customers
-SET cust_email = 'lincoln@us.gov'
-WHERE cust_name  = 'Abraham Lincoln';
-
-UPDATE customers
-SET cust_email = 'hello@napoleon.me'
-WHERE cust_name  = 'Napoleon Bonaparte';
+UPDATE customers SET cust_email = 'ppicasso@gmail.com' WHERE cust_name  = 'Pablo Picasso';
+UPDATE customers SET cust_email = 'lincoln@us.gov' WHERE cust_name  = 'Abraham Lincoln';
+UPDATE customers SET cust_email = 'hello@napoleon.me' WHERE cust_name  = 'Napoleon Bonaparte';
