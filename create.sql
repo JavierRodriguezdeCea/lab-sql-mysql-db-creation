5350830c9da12ae8bbd61ce812b30f50dc0dba6a
JavierRodriguezdeCea <javier.rodriguez.decea@outlook.com>
2024-11-22 09:36:44 +0100

    lab-creation-cambios

--- create.sql
+++ create.sql
@@ -4,38 +4,42 @@ USE lab_mysql;
 
 DROP TABLE IF EXISTS cars;
 CREATE TABLE IF NOT EXISTS cars (
-	vin INT PRIMARY KEY
-    manufacturer VARCHAR(50)
-    model VARCHAR(50)
-    year_date DATE
+	id_car INT,
+	vin INT PRIMARY KEY NOT NULL,
+    manufacturer VARCHAR(50),
+    model VARCHAR(50),
+    year_date DATE,
     color VARCHAR(50)
 );
+
 DROP TABLE IF EXISTS customers;
 CREATE TABLE IF NOT EXISTS customers (
-	customer_id INT PRIMARY KEY
-    name_customer  VARCHAR(50)
-    phone_number VARCHAR(50)
-    email VARCHAR(50)
-    color VARCHAR(50)
-    city VARCHAR(50)
-    state/province VARCHAR(50)
-    country VARCHAR(50)
-    zip/postal_code INT
+	customer_id INT PRIMARY KEY NOT NULL,
+    name_customer  VARCHAR(50),
+    phone_number VARCHAR(50),
+    email VARCHAR(50),
+    color VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    postal_code INT
 );
+
 DROP TABLE IF EXISTS salespersons;
 CREATE TABLE IF NOT EXISTS salespersons (
-	staff_id INT PRIMARY KEY
-    name_staff VARCHAR(50)
+	staff_id INT PRIMARY KEY NOT NULL,
+    name_staff VARCHAR(50),
     store VARCHAR(50)
 );
+
 DROP TABLE IF EXISTS invoices;
 CREATE TABLE IF NOT EXISTS invoices (
-	invoice_id INT PRIMARY KEY
-    date_day DATE
-    car INT
-    customer INT
-    salesperson INT
-    FOREIGN KEY (customer) REFERENCE customers(customer_id)
-	FOREIGN KEY (salesperson) REFERENCE salespersons(staff_id)
-    FOREIGN KEY (car) REFERENCE cars(vin)
+	invoice_id INT PRIMARY KEY NOT NULL,
+    date_day DATE,
+    car INT,
+    customer INT,
+    salesperson INT,
+    FOREIGN KEY (customer) REFERENCES customers(customer_id),
+	FOREIGN KEY (salesperson) REFERENCES salespersons(staff_id),
+    FOREIGN KEY (car) REFERENCES cars(vin)
 );
--- delete.sql
+++ delete.sql
@@ -1,3 +1,3 @@
 USE lab_mysql;
 
-DELETE FROM cars WHERE vin = 'DAM41UDN3CHU2WVF6';
+DELETE FROM cars WHERE vin = 'DAM41UDN3CHU2WVF6' and id_car = 5;
